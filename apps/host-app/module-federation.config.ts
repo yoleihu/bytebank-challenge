425898586add76d555b04b5a62af8ef761e6a698
Yolanda Ferreira de Souza <landacontato@gmail.com>
2025-08-02 17:30:28 -0300

    configura containers docker

--- apps/host-app/module-federation.config.ts
+++ apps/host-app/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['resume-account-mf'],
+  remotes: [['resume-account-mf', 'http://localhost:4201']],
 };
 
 /**
