bc56d64f0c885912774a4b5bdd4b6dcfcf78e24b
Yolanda Ferreira de Souza <landacontato@gmail.com>
2025-08-02 19:43:28 -0300

    remove logs

--- apps/host-app/module-federation.config.ts
+++ apps/host-app/module-federation.config.ts
@@ -1,18 +1,9 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
-
-const isProd = process.env.NODE_ENV === 'production';
-
-export const environment = {
-  production: isProd,
-  mfUrl: isProd ? process.env['NG_APP_MF_URL'] as string : 'http://localhost:4201',
-};
+import { environment } from './src/environments/environment';
 
 const config: ModuleFederationConfig = {
   name: 'host-app',
   remotes: [['resume-account-mf', environment.mfUrl]],
 };
-
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('NG_APP_MF_URL:', process.env['NG_APP_MF_URL']);
 
 export default config;
